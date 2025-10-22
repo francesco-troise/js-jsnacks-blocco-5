3a6db36237fbd283657673d2ae8a3d8c3fbcc40f
FrancescoTroise <francesco.troise98@gmail.com>
2025-10-22 17:12:06 +0200

    add j-snack6

--- ex-js-array-methods-main/snack-7/script.js
+++ ex-js-array-methods-main/snack-7/script.js
@@ -21,3 +21,8 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+let student = students.find(function(element){
+  return element.id === 2
+})
+console.log(student);
