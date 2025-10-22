1114ddf094a5cd45dc57f4aa5379665c5d066464
FrancescoTroise <francesco.troise98@gmail.com>
2025-10-22 16:21:41 +0200

    Add j-snack2

--- ex-js-array-methods-main/snack-2/script.js
+++ ex-js-array-methods-main/snack-2/script.js
@@ -6,3 +6,15 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+people.forEach(person =>{
+console.log(person.name);
+  
+})
+
+
+//Se volessi invece ottenere un array contenente i nomi delle persone, allora :
+
+const array_nomi = people.map(person => person.name)
+console.log(array_nomi)
+
