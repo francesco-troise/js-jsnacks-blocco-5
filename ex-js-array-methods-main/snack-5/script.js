0f9bfdb15328f67197e09c8d9b9cb454885e5364
FrancescoTroise <francesco.troise98@gmail.com>
2025-10-22 16:41:34 +0200

    add j-snak5

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -2,3 +2,12 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+function is_even(num){
+    if(num % 2 === 0){
+        return num
+    }
+}
+
+const array_even = nums.filter(is_even)
+console.log(array_even);
