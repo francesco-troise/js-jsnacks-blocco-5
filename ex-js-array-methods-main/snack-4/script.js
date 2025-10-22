2083c017e565628031b59119312d547d31473329
FrancescoTroise <francesco.troise98@gmail.com>
2025-10-22 16:28:25 +0200

    add j-snack4

--- ex-js-array-methods-main/snack-4/script.js
+++ ex-js-array-methods-main/snack-4/script.js
@@ -7,3 +7,6 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const array_name = posts.map(author => author.author)
+//(author => author.author) lasciarlo cosi' è diabolico, ma mi fa ridere
